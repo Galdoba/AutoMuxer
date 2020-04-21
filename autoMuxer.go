f9ddfa77caf7734532fe6f94e63747301a789fec
Galdoba <galdoba@mail.ru>
2020-04-21 20:43:51 +1000

    tags

--- autoMuxer.go
+++ autoMuxer.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"os/exec"
 	"strconv"
+	"strings"
+	"sync"
 	"time"
 
 	"github.com/Galdoba/utils"
@@ -17,12 +21,22 @@ const (
 	outPrefix    = "OUT_"
 	senderPrefix = "SENDER_"
 	taskFile     = "_TaskFile.txt"
+	taskCon0     = "TaskCon-0"
+	taskCon1     = "TaskCon-1"
+	taskCon2     = "TaskCon-2"
+	taskCon3     = "TaskCon-3"
+	taskCon4     = "TaskCon-4"
+	taskCon5     = "TaskCon-5"
+	taskCon6     = "TaskCon-6"
+	taskCon7     = "TaskCon-7"
+	taskDone     = "TaskCon-DONE"
 )
 
 var startTime time.Time
 var inFolder string
 var outFolder string
 var senderFolder string
+var taskFilePath string
 
 func check(e error) {
 	if e != nil {
@@ -97,7 +111,7 @@ func preCheck() {
 	checkFolder(outFolder)
 	checkFolder(senderFolder)
 	createTaskFile()
-
+	taskFilePath = inFolder + "\\" + taskFile
 	// if !fileAvailableM(inFolder, taskFile) {
 	// 	fmt.Println("Error: No", taskFile, "in location\n", inFolder)
 	// 	os.Exit(2)
@@ -110,53 +124,256 @@ func createTaskFile() {
 		f, err := os.Create(inFolder + "\\" + taskFile)
 		check(err)
 		f.Close()
+
 		fmt.Println(taskFile, "-- Created")
 	}
+
 }
 
 type Task struct {
-	inVideo     string
-	inDurat     string
-	offset      string
-	prVideo     string
-	prAudio     []string
-	prSubs      string
-	prTags      []string
-	outBaseName string
-	outDurat    string
-	status      string
+	dataLine        string
+	inVideo         string
+	inDurat         string
+	offset          string
+	prVideo         string
+	prAudio         []string
+	prSubs          string
+	prResolutionTag string
+	prAudioTag      string
+	prSubTag        string
+	outBaseName     string
+	outFullName     string
+	outDurat        string
+	status          int
+	inFilePos       int
+	outputTags      map[string]string
+}
+
+func mapTags(arg string) map[string]string {
+	tagMap := make(map[string]string)
+	argParts := strings.Split(arg, "__")
+	part1 := strings.Split(argParts[0], "_")
+	nameLen := len(part1) - 1
+	name := strings.Join(part1[:nameLen], "_")
+	tagMap["name"] = name
+	year := part1[nameLen:][0]
+	tagMap["year"] = year
+	part2 := strings.Split(argParts[1], "_")
+	for i := range part2 {
+		switch i {
+		default:
+			fmt.Println("Err??", part2[i])
+		case 0:
+			if validateTag(part2[i], resolutionTagWL()) {
+				tagMap["resolutionTag"] = part2[i]
+			}
+		case 1:
+			if validateTag(part2[i], audioTagWL()) {
+				tagMap["audioTag"] = part2[i]
+			}
+		case 2:
+			if validateTag(part2[i], subsTagWL()) {
+				tagMap["subsTag"] = part2[i]
+			}
+		}
+	}
+
+	return tagMap
 }
 
 func newTask(dataLine string) *Task {
 	task := &Task{}
-
-	task.inVideo = inFolder + "\\" + inVideo
-	task.outBaseName = get
+	task.dataLine = dataLine
+	task.outputTags = make(map[string]string)
+	args := task.args()
+	task.prResolutionTag, task.prAudioTag, task.prSubTag = searchTags(args[1])
+	task.outDurat = args[2]
+	task.offset = args[3]
+	task.inVideo = inFolder + "\\" + args[4]
+	task.outputTags = mapTags(args[1])
+	task.outBaseName = task.outputTags["name"] + "_" +
+		task.outputTags["year"] + "__" +
+		task.outputTags["resolutionTag"] + "_"
+	task.outFullName = args[1]
+	//fmt.Println("Test status", readStatus(dataLine))
+	//task.Info()
 	return task
+}
+
+func (task *Task) args() []string {
+	args := strings.Split(task.dataLine, " ")
+	return args
+}
+
+func readStatus(dataline string) int {
+	newStatus := 0
+	args := strings.Split(dataline, " ")
+	if len(args) < 1 {
+		return -1
+	}
+	if !strings.Contains(args[0], "TaskCon") {
+		return -2
+	}
+	return newStatus
+}
+
+func (task *Task) Info() {
+	fmt.Println("dataLine    string", task.dataLine)
+	fmt.Println("inVideo     string", task.inVideo)
+	fmt.Println("inDurat     string", task.inDurat)
+	fmt.Println("offset      string", task.offset)
+	fmt.Println("prVideo     string", task.prVideo)
+	fmt.Println("prAudio     []string", task.prAudio)
+	fmt.Println("prSubs      string", task.prSubs)
+	fmt.Println("prResTags   string", task.prResolutionTag)
+	fmt.Println("prAudTags   string", task.prAudioTag)
+	fmt.Println("prSubTags   string", task.prSubTag)
+	fmt.Println("outBaseName string", task.outBaseName)
+	fmt.Println("outFullName string", task.outFullName)
+	fmt.Println("outDurat    string", task.outDurat)
+	fmt.Println("status      int", task.status)
 }
 
 func formTask(line string) []string {
 	return []string{}
 }
 
-func readTaskFile() bool {
-	done := false
+func taskFileReadable() bool {
 	if !fileAvailableM(inFolder, taskFile) {
+		return false
+	}
+	// fileLines := utils.LinesFromTXT(taskFilePath)
+	// for i, _ := range fileLines {
+	// 	if fileLines[i] == "" {
+	// 		continue
+	// 	}
+	// 	args := dataLineArgs(i)
+	// 	if !statusValid(args[0]) {
+	// 		utils.EditLineInFile(taskFilePath, i, "TaskCon-0 "+fileLines[i])
+	// 	}
 
-		return done
+	// 	fmt.Println("DEBUG: line", i, "    Status valid =", statusValid(args[0]))
+	// 	if len(args) != 5 {
+	// 		fmt.Println("Error: line", i, "    Has", len(args), "arguments!")
+	// 		for e := range args {
+	// 			fmt.Println("  Arg", e, "'"+args[e]+"'")
+	// 		}
+	// 		fmt.Println("")
+	// 		continue
+	// 	}
+	// 	// fmt.Println(dataline)
+	// 	// fmt.Println(args)
+	// 	// fmt.Println("------")
+	// }
+	return true
+}
+
+func dataLineArgs(i int) []string {
+	lines := utils.LinesFromTXT(taskFilePath)
+	args := strings.Split(lines[i], " ")
+	return args
+}
+
+func statusValid(status string) bool {
+	switch status {
+	default:
+		return false
+	case taskCon0, taskCon1, taskCon2, taskCon3, taskCon4, taskCon5, taskCon6, taskDone:
+		return true
 	}
-	path := inFolder + "\\" + taskFile
-	fileLines := utils.LinesFromTXT(path)
-	for i, v := range fileLines {
-		fmt.Println("Line", i, " -- ", v)
+}
+
+func resolutionTagWL() []string {
+	return []string{"sd", "hd", "3d"}
+}
+
+func audioTagWL() []string {
+	return []string{"ar2e2", "ar6e2", "ar2e6", "ar6e6",
+		"ar6", "ar2",
+		"ae6", "ae2",
 	}
-	done = true
-	return done
+}
+
+func subsTagWL() []string {
+	return []string{"sr", "[NO_SUBS]"}
+
+}
+
+func validateTag(tag string, whiteList []string) bool {
+	for i := range whiteList {
+		if tag == whiteList[i] {
+			return true
+		}
+	}
+	return false
+}
+
+func searchTags(arg string) (resolutionTag string, audioTag string, subsTag string) {
+	resolutionTag = "INVALID"
+	audioTag = "INVALID"
+	subsTag = "INVALID"
+	argParts := strings.Split(arg, "__")
+	tags := strings.Split(argParts[1], "_")
+	if len(tags) == 2 {
+		subsTag = "[NO_SUBS]"
+	}
+	for i := range tags {
+		switch i {
+		default:
+			fmt.Println("Err??", tags[i])
+		case 0:
+			if validateTag(tags[i], resolutionTagWL()) {
+				resolutionTag = tags[i]
+			}
+		case 1:
+			if validateTag(tags[i], audioTagWL()) {
+				audioTag = tags[i]
+			}
+		case 2:
+			if validateTag(tags[i], subsTagWL()) {
+				subsTag = tags[i]
+			}
+		}
+	}
+
+	return resolutionTag, audioTag, subsTag
+}
+
+func checkArgs(args []string) bool {
+	if len(args) != 5 {
+		fmt.Println("Error:", args, "have", len(args), "arguments (expecting 5)")
+		return false
+	}
+	fmt.Println("StatusArg:", args[0])
+
+	fmt.Println("ResultArg:", args[1])
+	rTag, aTag, sTag := searchTags(args[1])
+	fmt.Println("     Tags:", rTag, aTag, sTag)
+	return true
 }
 
 func main() {
 	preCheck()
-	fmt.Println(readTaskFile())
+	if !taskFileReadable() {
+		fmt.Println("TaskFile is not readable...")
+		fmt.Println("Resolve and restart")
+		return
+	}
+	for i := 0; i < 1000; i++ {
+		time.Sleep(time.Second * 3)
+		utils.ClearScreen()
+		curTime := time.Now()
+		fmt.Println("cycle", i)
+		fmt.Println("Start time :", startTime)
+		fmt.Println("Curent time:", curTime)
+		dataLines := dataLines()
+		for i := range dataLines {
+			task := newTask(dataLines[i])
+			task.Info()
+		}
+
+	}
+
 	return
 	// err = os.Mkdir("subdir", 0755)
 	// check(err)
@@ -239,4 +456,151 @@ func fileExists(folder, file string) bool {
 		return true
 	}
 	return false
+}
+
+func dataLines() (result []string) {
+	lines := utils.LinesFromTXT(taskFilePath)
+	for _, dataLine := range lines {
+		result = append(result, dataLine)
+	}
+	return result
+}
+
+/*
+Status:
+TaskCon
+
+TaskCon			| 1 | 2 | 3 | 4 | 5 |
+конечная задача	| + | + | + | + | + | - readTask() - есть ли ошибки в чтении таска
+ИСХвидео		| - | + | + | + | + | - checkInput() - свободен ли видеофайл InVideo в папке INfolder
+ОБРвидео		| - | - | + | + | + | - checkPrVideo() - свободен ли видеофайл shortname.mp4 в папке PRfolder
+ОБРзвук 		| - | - | - | + | + | - checkAudio() - свободен ли аудиофайл/ы shortname.aac в папке PRfolder
+ОБРсабы			| - | - | - | - | + | - checkSubs() - свободен ли файл сабов в папке PRfolder с тегом Sync
+
+TaskCon-0
+readTask():
+получает ShortName; tags; имена предмуксовых видео и аудио; проверка (длинну файла)
+TaskCon-1
+TaskCon-2
+TaskCon-3
+TaskCon-4
+TaskCon-5
+
+
+
+
+
+
+*/
+
+func videoDuration(folder, file string) string {
+	if fileAvailableM(folder, file) {
+		cmd := exec.Command("ffmpeg", "-i", file)
+
+		output, _ := cmd.CombinedOutput()
+		stringOUT := string(output)
+		str1 := strings.Split(stringOUT, "Duration: ")
+		time.Sleep(time.Second * 1)
+		if len(str1) > 0 {
+			durationSTR := strings.Split(str1[1], ", ")
+			return durationSTR[0]
+		}
+	}
+	//cmd.Stdout = os.Stdout
+	//cmd.Stderr = os.Stderr
+	//cmd.Run()
+	return "00:00:00.00"
+}
+
+func ffToPrem(duration string) string {
+	premiereDur := ""
+	timeArgs := strings.Split(duration, ":")
+	hours, errHour := strconv.Atoi(timeArgs[0])
+	if errHour != nil {
+		panic(errHour)
+	}
+	minutes, errMin := strconv.Atoi(timeArgs[1])
+	if errMin != nil {
+		panic(errMin)
+	}
+	secs, err := strconv.ParseFloat(timeArgs[2], 64)
+	if err != nil {
+		panic(err)
+	}
+	secsInt := int(secs)
+	strHour := strconv.Itoa(hours)
+	if hours < 10 {
+		strHour = "0" + strHour
+	}
+	strMin := strconv.Itoa(minutes)
+	if minutes < 10 {
+		strMin = "0" + strMin
+	}
+	strSec := strconv.Itoa(secsInt)
+	if secsInt < 10 {
+		strSec = "0" + strSec
+	}
+	part := secs
+	for part > 1 {
+		part = part - 1
+	}
+	part = toFixed(part, 3)
+	part = part / 0.04
+	frame := int(part)
+	strFrame := strconv.Itoa(frame)
+	if frame < 10 {
+		strFrame = "0" + strFrame
+	}
+	if frame == 25 {
+		strFrame = "00"
+	}
+	premiereDur = strHour + ":" + strMin + ":" + strSec + ":" + strFrame
+	return premiereDur
+}
+
+func floatToString(input_num float64) string {
+	// to convert a float number to a string
+	return strconv.FormatFloat(input_num, 'f', 2, 64)
+}
+
+func round(num float64) int {
+	return int(num + math.Copysign(0.5, num))
+}
+
+func toFixed(num float64, precision int) float64 {
+	output := math.Pow(10, float64(precision))
+	return float64(round(num*output)) / output
+}
+
+func premToFF(duration string) string {
+	parts := strings.Split(duration, ":")
+	secsInt, _ := strconv.Atoi(parts[2])
+	partsInt, _ := strconv.Atoi(parts[3])
+	partsFl := float64(partsInt)*40/1000 + float64(secsInt)
+	sec := floatToString(partsFl)
+	return parts[0] + ":" + parts[1] + ":" + sec
+}
+
+func premToFrames(duration string) int {
+	parts := strings.Split(duration, ":")
+	hour, _ := strconv.Atoi(parts[0])
+	min, _ := strconv.Atoi(parts[1])
+	sec, _ := strconv.Atoi(parts[2])
+	frm, _ := strconv.Atoi(parts[3])
+	return frm + 25*sec + 1500*min + 90000*hour
+}
+
+func exe_cmd(cmd string, wg *sync.WaitGroup) {
+	fmt.Println("command is ", cmd)
+	// splitting head => g++ parts => rest of the command
+	parts := strings.Fields(cmd)
+	head := parts[0]
+	parts = parts[1:len(parts)]
+
+	out, err := exec.Command(head, parts...).Output()
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	fmt.Printf("%s", out)
+	wg.Done() // Need to signal to waitgroup that this goroutine is done
 }
